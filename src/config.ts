1acafa8c0d020c1bacc871941e6de19b556ecb2f
Philipp Gröschler <phgroe@gmx.net>
2018-10-04 07:38:59 +0200

    Generic convict config finalizer

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+import { Config } from 'convict';
+
+import { existsSync } from 'fs';
+import { join } from 'path';
+import { rootPath } from '.';
+
+
+type FinalizeCallback <S> = (schema : Config <S>) => void;
+
+export function finalizeConfig <S> (
+	schema : Config <S>,
+	callback? : FinalizeCallback <S>
+) : S
+{
+	const configFile = join (rootPath, 'config.json5');
+
+	if (existsSync (configFile))
+	{
+		try {
+			schema.loadFile (configFile);
+		}
+		catch (error) {
+			console.error ('Error while trying to load %s', configFile);
+			console.error (error);
+		}
+	}
+
+	if (callback) {
+		callback (schema);
+	}
+
+	schema.validate({
+		allowed: 'strict',
+	});
+
+	return Object.freeze (schema.getProperties ());
+}
