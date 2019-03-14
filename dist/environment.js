af9e8a36513ee67bc8e398058991601c7923a1a6
Philipp Gröschler <philipp.groeschler@dn.no>
2019-03-14 08:46:10 +0100

    Compile it!

--- dist/environment.js
+++ dist/environment.js
@@ -3,12 +3,12 @@ var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 _a = (() => {
     let env = process.env.NODE_ENV || '';
-    if (0 > ['live', 'develop', 'test'].indexOf(env)) {
+    if (0 > ['develop', 'live', 'production', 'test'].indexOf(env)) {
         env = 'develop';
     }
     return {
         isDevelop: env === 'develop',
-        isLive: env === 'live',
+        isLive: env === 'live' || env === 'production',
         isTest: env === 'test',
     };
 })(), exports.isDevelop = _a.isDevelop, exports.isLive = _a.isLive, exports.isTest = _a.isTest;
